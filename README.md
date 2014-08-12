c2a8851c8f9cef00b224a71f7233af44356b7f1f
Anders Janmyr <anders.janmyr@sonymobile.com>
2014-08-12 08:52:32 +0200

    Added readme and some comments

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nintynine
+
+Clojure solutions to [Ninety-Nine Scala Problems](http://aperiodic.net/phil/scala/s-99/)
+
+## Usage
+
+    # Run all tests
+    $ lein midje
+
+    # Autotest
+    $ lein midje :autotest
+
--- test/nintynine/core_test.clj
+++ test/nintynine/core_test.clj
@@ -1,31 +1,30 @@
 (ns nintynine.core-test
-  (:require [midje.sweet :refer :all]
-            [nintynine.core :refer :all]))
+  (:require [midje.sweet :refer :all]))
 
-(fact "last element"
+(fact "P01: last element"
   (last (list 1 2 3)) => 3
   (last [1 2 3]) => 3
   )
 
-(fact "last but one"
+(fact "P02: last but one"
   (last (butlast (list 1 2 3))) => 2
   (-> (list 1 2 3) butlast last) => 2
   (-> [1 2 3] butlast last) => 2)
 
-(fact "nth element"
+(fact "P03: nth element"
   (nth '(1 2 3) 0) => 1
   (nth [1 2 3] 0) => 1)
 
-(fact "number of elements"
+(fact "P04: number of elements"
   (count '(1 2 3)) => 3)
 
-(fact "reversed"
+(fact "P05: reversed"
   (reverse '(1 2 3)) => '(3 2 1)
   (reverse [1 2 3]) => [3 2 1])
 
 (defn palindrome [l]
   (= (reverse l) l))
 
-(fact "palindrome"
+(fact "P06: palindrome"
   (palindrome [1 2 3 2 1]) => true
   (palindrome [1 2 3 2 3]) => false)
